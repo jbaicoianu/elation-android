f89c64314dd6db197bacdb23fd8360ee68c88445
James Baicoianu <james_github@baicoianu.com>
2014-02-16 02:09:56 -0800

    Added color for console errors, warnings, etc

--- src/com/elation/demo/ElationDebugConsoleMessageAdapter.java
+++ src/com/elation/demo/ElationDebugConsoleMessageAdapter.java
@@ -1,6 +1,7 @@
 package com.elation.demo;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.LayoutInflater;
@@ -8,14 +9,22 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.webkit.ConsoleMessage;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ElationDebugConsoleMessageAdapter extends ArrayAdapter<ConsoleMessage> {
 
     private Context context;
 
+    private HashMap<ConsoleMessage.MessageLevel, Integer> messageColors = new HashMap<ConsoleMessage.MessageLevel, Integer>();;
+
     public ElationDebugConsoleMessageAdapter(Context context, int textViewResourceId, ArrayList<ConsoleMessage> items) {
         super(context, textViewResourceId, items);
         this.context = context;
+
+        messageColors.put(ConsoleMessage.MessageLevel.DEBUG, Color.CYAN);
+        messageColors.put(ConsoleMessage.MessageLevel.ERROR, Color.RED);
+        messageColors.put(ConsoleMessage.MessageLevel.LOG, Color.GRAY);
+        messageColors.put(ConsoleMessage.MessageLevel.WARNING, Color.YELLOW);
     }
 
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -31,6 +40,12 @@ public class ElationDebugConsoleMessageAdapter extends ArrayAdapter<ConsoleMessa
             TextView message = (TextView) view.findViewById(R.id.message);
             if (message != null) {
                 message.setText(item.message());
+                ConsoleMessage.MessageLevel msglvl = item.messageLevel();
+                if (messageColors.containsKey(msglvl)) {
+                  message.setTextColor(messageColors.get(msglvl));
+                } else {
+                  message.setTextColor(Color.GRAY);
+                }
             }
             TextView location = (TextView) view.findViewById(R.id.location);
             if (location != null) {
